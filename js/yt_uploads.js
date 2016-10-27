217a36d072e7aa3219958f99cd47d4c8ea0a8e86
Isaac 'Yak' L <webmaster@izacktheyak.com>
2016-10-26 21:17:18 -0700

    First commit

---
+++ js/yt_uploads.js
@@ -0,0 +1,48 @@
+var channelId = 'UCY_WxlLSJkAX9bBLetuyy8g';
+var channelIdYak = 'UChtsf8MNcRKILfxEGZdmnWQ';
+
+$(document).ready(function(){
+	
+	/*$.get(
+		"https://www.googleapis.com/youtube/v3/channels",{
+			part: 'contentDetails',
+			id: channelId,
+			key: 'AIzaSyD6NLkxcJHA3ocC_VdfklfKStcRxQRHBJk'},
+			function(data){
+				$.each(data.items, function(i, item){
+					pid = item.contentDetails.relatedPlaylists.uploads;
+					getVids(pid);
+				})
+			}
+	);*/
+	
+	function getVids(){
+		$.get(
+		"https://www.googleapis.com/youtube/v3/playlistItems",{
+			part: 'snippet',
+			maxResults: 12,
+			playlistId: 'PLmsEFKXc2nPXnsnc97hKRLe-Ew9n8IAuv',
+			key: 'AIzaSyD6NLkxcJHA3ocC_VdfklfKStcRxQRHBJk'},
+			function(data){
+				var output;
+				if(data.items.length>0){
+					$('#haxvidsstatus').html('&nbsp;&nbsp;&nbsp;Loading Videos...');
+					$.each(data.items, function(i, item){
+						videoTitle = item.snippet.title;
+						videoId = item.snippet.resourceId.videoId;
+						
+						output = '<div class="work"><div class="embed-container"><iframe onload="$(\'#haxvidsstatus\').remove();" src="https://www.youtube.com/embed/' + videoId + '?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe></div></div>';
+						$('#haxvids').append(output);
+					})
+				}
+				else
+				{
+					$('#haxvidsstatus').html('&nbsp;&nbsp;&nbsp;No Videos. Feed me!');
+				}
+			}
+		);
+	}
+	
+	getVids();
+	
+});
