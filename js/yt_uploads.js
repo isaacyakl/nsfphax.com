3238ac899f128b54ba12eeb664f89f84e505417c
Isaac "Yak" L <i@isaacyakl.com>
2020-02-17 15:14:03 -0800

    Update key and clean up code

--- js/yt_uploads.js
+++ js/yt_uploads.js
@@ -1,69 +1,49 @@
 var channelId = "UCY_WxlLSJkAX9bBLetuyy8g";
 
-var channelIdYak = "UChtsf8MNcRKILfxEGZdmnWQ";
+function getVids() {
+  $.get(
+    "https://www.googleapis.com/youtube/v3/playlistItems",
+    {
+      part: "snippet",
+      maxResults: 12,
+      playlistId: "PLmsEFKXc2nPXnsnc97hKRLe-Ew9n8IAuv",
+      key: apiKey // set this variable to your API key in "key.js"
+    },
 
-$(document).ready(function() {
-  /*$.get(
+    function(data) {
+      var output;
 
-		"https://www.googleapis.com/youtube/v3/channels",{
+      if (data.items.length > 0) {
+        // Videos were returned
+        $("#haxvidsstatus").html("Loading...");
 
-			part: 'contentDetails',
+        $.each(data.items, function(i, item) {
+          videoTitle = item.snippet.title;
 
-			id: channelId,
+          videoId = item.snippet.resourceId.videoId;
 
-			key: 'AIzaSyD6NLkxcJHA3ocC_VdfklfKStcRxQRHBJk'},
+          output =
+            '<div class="work"><div class="embed-container"><iframe onload="$(\'#haxvidsstatus\').remove();" src="https://www.youtube.com/embed/' +
+            videoId +
+            '?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe></div></div>';
 
-			function(data){
-
-				$.each(data.items, function(i, item){
-
-					pid = item.contentDetails.relatedPlaylists.uploads;
-
-					getVids(pid);
-
-				})
-
-			}
-
-	);*/
-
-  function getVids() {
-    $.get(
-      "https://www.googleapis.com/youtube/v3/playlistItems",
-      {
-        part: "snippet",
-
-        maxResults: 12,
-
-        playlistId: "PLmsEFKXc2nPXnsnc97hKRLe-Ew9n8IAuv",
-
-        key: "AIzaSyBkYV1U47glzInyBNtIf8TBOg4xYgqg_S4"
-      },
-
-      function(data) {
-        var output;
-
-        if (data.items.length > 0) {
-          $("#haxvidsstatus").html("&nbsp;&nbsp;&nbsp;Loading Videos...");
-
-          $.each(data.items, function(i, item) {
-            videoTitle = item.snippet.title;
-
-            videoId = item.snippet.resourceId.videoId;
-
-            output =
-              '<div class="work"><div class="embed-container"><iframe onload="$(\'#haxvidsstatus\').remove();" src="https://www.youtube.com/embed/' +
-              videoId +
-              '?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe></div></div>';
-
-            $("#haxvids").append(output);
-          });
-        } else {
-          $("#haxvidsstatus").html("&nbsp;&nbsp;&nbsp;No Videos. Feed me!");
-        }
+          $("#haxvids").append(output);
+        });
+      } else {
+        // No videos found
+        $("#haxvidsstatus").html(
+          "No Videos. Feed me!<br><img src='https://media.giphy.com/media/uAEXaY2T99zeE/giphy.gif'>"
+        );
       }
+    }
+  ).error(function() {
+    $("#haxvidsstatus").html(
+      "OOF! Failed to load videos. Try again in a couple hours.<br><img src='https://media.giphy.com/media/rZxMZGtDcKdS8/giphy.gif'>"
     );
-  }
+  });
+}
 
-  getVids();
+// Once page is loaded
+$(document).ready(function() {
+  getVids(); // Show videos
 });
