ba286c519c8b675a51cb8fa18ecca0fc85df57e8
Isaac "Yak" L <i@isaacyakl.com>
2022-07-26 22:59:42 -0700

    Add API key

--- js/yt_uploads.js
+++ js/yt_uploads.js
@@ -1,49 +1,42 @@
 var channelId = "UCY_WxlLSJkAX9bBLetuyy8g";
 
 function getVids() {
-  $.get(
-    "https://www.googleapis.com/youtube/v3/playlistItems",
-    {
-      part: "snippet",
-      maxResults: 12,
-      playlistId: "PLmsEFKXc2nPXnsnc97hKRLe-Ew9n8IAuv",
-      key: apiKey // set this variable to your API key in "key.js"
-    },
+	$.get(
+		"https://www.googleapis.com/youtube/v3/playlistItems",
+		{
+			part: "snippet",
+			maxResults: 12,
+			playlistId: "PLmsEFKXc2nPXnsnc97hKRLe-Ew9n8IAuv",
+			key: "AIzaSyD385MdHyR2kzNgvAYPrtWjpuzSGtejML0",
+		},
 
-    function(data) {
-      var output;
+		function (data) {
+			var output;
 
-      if (data.items.length > 0) {
-        // Videos were returned
-        $("#haxvidsstatus").html("Loading...");
+			if (data.items.length > 0) {
+				// Videos were returned
+				$("#haxvidsstatus").html("Loading...");
 
-        $.each(data.items, function(i, item) {
-          videoTitle = item.snippet.title;
+				$.each(data.items, function (i, item) {
+					videoTitle = item.snippet.title;
 
-          videoId = item.snippet.resourceId.videoId;
+					videoId = item.snippet.resourceId.videoId;
 
-          output =
-            '<div class="work"><div class="embed-container"><iframe onload="$(\'#haxvidsstatus\').remove();" src="https://www.youtube.com/embed/' +
-            videoId +
-            '?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe></div></div>';
+					output = '<div class="work"><div class="embed-container"><iframe onload="$(\'#haxvidsstatus\').remove();" src="https://www.youtube.com/embed/' + videoId + '?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe></div></div>';
 
-          $("#haxvids").append(output);
-        });
-      } else {
-        // No videos found
-        $("#haxvidsstatus").html(
-          "No Videos. Feed me!<br><img src='https://media.giphy.com/media/uAEXaY2T99zeE/giphy.gif'>"
-        );
-      }
-    }
-  ).error(function() {
-    $("#haxvidsstatus").html(
-      "OOF! Failed to load videos. Try again in a couple hours.<br><img src='https://media.giphy.com/media/rZxMZGtDcKdS8/giphy.gif'>"
-    );
-  });
+					$("#haxvids").append(output);
+				});
+			} else {
+				// No videos found
+				$("#haxvidsstatus").html("No Videos. Feed me!<br><img src='https://media.giphy.com/media/uAEXaY2T99zeE/giphy.gif'>");
+			}
+		}
+	).error(function () {
+		$("#haxvidsstatus").html("OOF! Failed to load videos. Try again in a couple hours.<br><img src='https://media.giphy.com/media/rZxMZGtDcKdS8/giphy.gif'>");
+	});
 }
 
 // Once page is loaded
-$(document).ready(function() {
-  getVids(); // Show videos
+$(document).ready(function () {
+	getVids(); // Show videos
 });
